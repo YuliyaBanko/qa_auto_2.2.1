b8cecccaa89c46b922708f37dbdefc2c96526474
Yuliya Banko <yuliyabanko@gmail.com>
2024-09-08 11:06:46 +0300

    Homework 2.2.1 Изменения в код

--- calculations.js
+++ calculations.js
@@ -3,16 +3,19 @@ function increaseBalance(x, y) {
   return x;
 }
 
-function decreaseBalance(x) {
+function decreaseBalance(x,y) {
 x = x - y;
 return x;
 }
+// "y" was not defined
 
 function divideBalanceByAccounts(x, y) {
   x = x / y;
-  return x;
   console.log("x");
+  return x;
 }
+// после метода return никакой код восприниматься не будет, console.log("x") выносим вперед;
+// не очень понятна смысловая нагрузка выведения в консоль переменной х в качестве строки(?)
 
 function getRestAfterDivision(x, y) {
   x = x % y;
@@ -21,5 +24,5 @@ function getRestAfterDivision(x, y) {
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(divideBalanceByAccounts(3000, 2));// в функции divideBalanceByAccount было 2 параметра, третий аргумент не несет нагрузку, надо либо менять функцию, либо убирать его из строки выведения
 console.log(getRestAfterDivision(7000, 3));
